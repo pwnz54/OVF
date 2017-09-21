ce44691fb702fc518af5c7f49390e5c7f5aff7e0
pwnz54 <kas154723@gmail.com>
2017-09-21 15:45:45 +0700

    Update task3_1.cpp

--- task3_1.cpp
+++ task3_1.cpp
@@ -7,6 +7,10 @@ double func(double x)
 {
 	return tan(x) - x;
 }
+double dfunc(double x)
+{
+	return (1 / (cos(x)*cos(x)) - 1);
+}
 double accuracy(double a, double b, double eps)
 {
 	return log2((b - a) / eps) - 1;
@@ -24,13 +28,27 @@ double dichotomy(double a, double b, double c, double eps)
 	cout << (a + b) / 2;
 	return 0;
 }
+double newton(double eps, double x0)
+{
+	double x;
+	x = x0 - func(x0) / dfunc(x0);
+	while (abs(x - x0) > eps)
+	{
+		x0 = x;
+		x = x0 - (func(x0) / dfunc(x0));
+		
+	}
+	cout << x;
+	return 0;
+} 
 int main()
 {
 	double a = -PI;
 	double b = PI;
-	double eps = 0.001; 
+	double eps = 0.01; 
 	double c = 0;
-	dichotomy(a, b, c, eps);
-	
+//	dichotomy(a, b, c, eps);
+	newton(0.001, 10.9);
+
 	system("pause");
 }
