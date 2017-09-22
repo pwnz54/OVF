e0472828440a55df7ebc517687dee3c81fa7d3b1
pwnz54 <kas154723@gmail.com>
2017-09-22 17:56:05 +0700

    Update task3.cpp

--- task3.cpp
+++ task3.cpp
@@ -7,21 +7,17 @@ double poly(double x)
 }
 double accuracy(double a, double b, double eps)
 {
-	return log2((b - a) / eps + 1);
+	double n = log2((b - a) / eps)-1;
+	//cout << n << endl;
+	return n;
 }
-int main()
+double polynom(double a, double b, double eps)
 {
-	double a;
-	double b;
 	double c;
-	cout << "Input range: [a,b] \n";
-	cin >> a >> b;
-
-	
-	double eps = 0.001;
 	for (int i = 0; i < accuracy(a, b, eps); i++)
 	{
 		c = (a + b) / 2;
+
 		if (poly(a)*poly(c) <= 0)
 		{
 
@@ -31,8 +27,44 @@ int main()
 		if (poly(b)*poly(c) <= 0)
 			a = c;
 		//cout << (a + b) / 2 << endl;
+		cout << i << endl;
 	}
 	cout << (a + b) / 2 << endl;
+	return 0;
+}
+double pl(double a, double b, double eps)
+{
+	double c;
+	int i = 0;
+	while (abs(a - b) > eps)
+	{
+		i++;
+		c = (a + b) / 2;
+		if (poly(a)*poly(c) <= 0)
+		{
+
+			b = c;
+
+		}
+		if (poly(b)*poly(c) <= 0)
+			a = c;
+		cout << i<<endl;
+	}
+	cout << (a + b) / 2;
+	return 0;
+}
+int main()
+{
+	double a;
+	double b;
+	double c;
+	cout << "Input range: [a,b] \n";
+	cin >> a >> b;
+	double eps = 0.001;
+	polynom(a, b, eps);
+	cout << "AGAIN \n";
+	pl(a, b, eps);
+
 	system("pause");
 
 }
