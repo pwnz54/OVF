98cc736e59baaa0fac76e38a7d3582fcd4382ab8
pwnz54 <kas154723@gmail.com>
2017-10-13 19:26:55 +0700

    Update task3_2.cpp

--- task3_2.cpp
+++ task3_2.cpp
@@ -1,89 +1,91 @@
 #include<iostream>
 #include<cmath>
 #define PI 3.14159
-
+#define I 1.29587
 using namespace std;
-
-double func(double x)
+double f(double x)
 {
-	return tan(x) - x;
+	return (1 / (1 + x*x));
 }
-
-double dfunc(double x)
+double f1(double x)
 {
-	return (1 / (cos(x)*cos(x)) - 1);
+	return exp(sin(x))*pow(x, 1.0 / 3);
 }
-
-double accuracy(double a, double b, double eps)
-{
-	return log2((b - a) / eps) - 1;
+double net(double a, double b, int n){
+	return (b - a) / n;
 }
-double d2(double x)
+double simp(double a, double b, double(*f)(double), int n)
 {
-	return (2 * tan(x)*pow(1/cos(x), 2));
-}
-double newton(double eps,int n)
-{
-	int i = 0;
-	double x = 0;
-	//double x0 = PI*(n+1) - PI / 2. + 0.01;
-	double x1 = PI*(n + 1) - PI / 2.;
-	double x0;
-	if (d2(x1) > 0)
+	double h = net(a, b, n);
+	double sum = 0;
+	double x0 = a;
+	double x1 = x0 + h;
+	for (int i(0); i < n; i++)
 	{
-		x0 = x1 - 0.01;
+		sum = sum + f(x0) + 4 * f(x0 + h / 2) + f(x1);
+		x0 = x0 + h;
+		x1 = x1 + h;
 	}
-	if (d2(x1) < 0)
-	{
-		x0 = x1 + 0.01;
-	}
-	x = x0 - func(x0) / dfunc(x0);
-	while (abs(x - x0) > eps)
-	{
-		x0 = x;
-		x = x0 - (func(x0) / dfunc(x0));
-		i++;
-		cout << i << endl;
-		cout << x << endl;
-	}
-	return 0;
+	return (h / 6)*sum;
 }
-double dichotomy(double a, double b, double eps)
+double trap(double a, double b, double(*f)(double), int n)
 {
-	double c = 0;
-	for (int i(0); i < accuracy(a, b, eps); i++)
+	double step = net(a, b, n);
+	double s = 0;
+	double x = a;
+	while (x < b)
 	{
-		c = (a + b) / 2;
-		if (func(a)*func(c) <= 0)
-			b = c;
-		if (func(b)*func(c) <= 0)
-			a = c;
+		s = s + (f(x + step) + f(x))*step / 2;
+		x = x + step;
 	}
-	return (a + b) / 2;
-}
-double simple_iter(double x0, double eps)
-{
-	int i = 0;
-	double x = 0;
-	x = atan(x0);
-	while (abs(x - x0) > eps)
-	{
-		x0 = x;
-		x = atan(x0);
-	}
-	//cout << i << endl;
-	return x;
+	return s;
 }
 int main()
-{
-	double a = -PI;
-	double b = PI;
-	double eps = 0.1;
+{	
 	
-	//cout << dichotomy(4, 5, 0.01)<<endl;	
-	
-	newton(0.001, -10);
-	//cout << simple_iter(4, 0.01);
-	//cout << newton(0.01, 10);
+	for (int n = 4; n <= 4096; n = n*2)
+	{
+		cout << n << endl;
+		cout << abs(trap(-1, 1, f, n) - PI / 2) << endl;
+	}
+	/*cout << "dx/(1+x^2) from -1 to 1 \n Simpson: \n";
+	cout << simp(-1, 1, f, 4) << endl;
+	cout << simp(-1, 1, f, 8) << endl;
+	cout << simp(-1, 1, f, 16) << endl;
+	cout << "Trapecia: \n";
+	cout << trap(-1, 1, f, 4) << endl;
+	cout << trap(-1, 1, f, 8) << endl;
+	cout << trap(-1, 1, f, 16) << endl;
+	cout << "second integr from 0 to 1 \n Simpson \n";
+	cout << simp(0, 1, f1, 4) << endl;
+	cout << simp(0, 1, f1, 8) << endl;
+	cout << simp(0, 1, f1, 16) << endl;
+	cout << "trapecia \n";
+	cout << trap(0, 1, f1, 4) << endl;
+	cout << trap(0, 1, f1, 8) << endl;
+	cout << trap(0, 1, f1, 16) << endl;
+	cout << "DELTA: \n 1 integr simps \n";
+	cout << simp(-1, 1, f, 4) - simp(-1, 1, f, 4096) << endl;
+	cout << simp(-1, 1, f, 8) - simp(-1, 1, f, 4096) << endl;
+	cout << simp(-1, 1, f, 16) - simp(-1, 1, f, 4096) << endl;
+	cout << "trapec 1\n";
+	cout << trap(-1, 1, f, 4) - trap(-1,1,f,4096) << endl;
+	cout << trap(-1, 1, f, 8) - trap(-1, 1, f, 4096) << endl;
+	cout << trap(-1, 1, f, 16) - trap(-1, 1, f, 4096) << endl;
+	cout << "2nd integral \n simp \n";
+	cout << simp(0, 1, f1, 4) - simp(0, 1, f1, 4096) << endl;
+	cout << simp(0, 1, f1, 8) - simp(0, 1, f1, 4096) << endl;
+	cout << simp(0, 1, f1, 16) - simp(0, 1, f1, 4096) << endl;
+	cout << "2nd integral \n trapec \n";
+	cout << trap(0, 1, f1, 4) - trap(0, 1, f1, 4096) << endl;
+	cout << trap(0, 1, f1, 8) - trap(0, 1, f1, 4096) << endl;
+	cout << trap(0, 1, f1, 16) - trap(0, 1, f1, 4096) << endl;
+	*/
+
+
+
+
+
 	system("pause");
+	return 0;
 }
