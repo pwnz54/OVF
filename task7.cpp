db55601a5050ade135663540e3ef9107b0d0353d
pwnz54 <kas154723@gmail.com>
2017-11-16 14:12:17 +0700

    Update task7.cpp

--- task7.cpp
+++ task7.cpp
@@ -1,80 +1,55 @@
 #include<iostream>
-#include<cmath>
-#define R 0.049787068367863942979342415650061776631699592188423215567
-#define ALPH 0.75
+#include<fstream>
 
+#define ALPH 0.75
 using namespace std;
-double h(int n, double *t)
-{
-	return t[n] - t[n-1];
-}
+
 double f(double x, double t)
 {
-	return -x+t*0;
-}
-double* net(double a, double b, int n)
-{
-	double *t = new double[n + 1];
-	for (int i = 0; i <= n; i++)
-	{
-		t[i] = a + (b - a)*i / double(n);
-	}
-	return t;
+	return -x;
 }
 
-double euler(double (*f)(double,double), double* mas, int n, double step)
+void euler(double(*f)(double, double), double a, double b, double t_init, double x_init, double n)
 {
-	double t_start = mas[0];
-	double t_final = mas[n];
+	double t_start = t_init;
+	double x_start = x_init;
+	double x_cur = x_start;
 	double t_cur = t_start;
-	double x_init = 1;
-	double x_cur = x_init;
 	double x_next = 0;
-	while (t_cur <= t_final)
+	double step = (b - a) / n;
+	ofstream data("euler.txt");
+	while (t_cur <= b)
 	{
 		x_next = x_cur + step*f(x_cur, t_cur);
 		x_cur = x_next;
+		data << t_cur << '\t' << x_cur << endl;
 		t_cur += step;
 	}
-	return x_cur;
+	data.close();
 }
-
-double runge(double(*f)(double, double), double* mas, int n, double step)
+void runge(double(*f)(double, double), double a, double b, double t_init, double x_init, double n)
 {
-	double t_start = mas[0];
-	double t_final = mas[n];
+	double t_start = t_init;
+	double x_start = x_init;
+	double x_cur = x_start;
 	double t_cur = t_start;
-	double x_init = 1;
-	double x_cur = x_init;
 	double x_next = 0;
-	while (t_cur <= t_final)
+	double step = (b - a) / n;
+	ofstream data("runge.txt");
+	while (t_cur <= b)
 	{
 		x_next = x_cur + step*((1 - ALPH)*f(x_cur, t_cur) + ALPH*f(x_cur + step / (2 * ALPH), t_cur + step / (2 * ALPH)*f(x_cur, t_cur)));
 		x_cur = x_next;
+		data << t_cur << '\t' << x_cur << endl;
 		t_cur += step;
 	}
-	return x_cur;
+	data.close();
 }
 int main()
 {
 	int n;
 	cin >> n;
-	double* mas = net(0, 3, n);
-	double step = h(n, mas);
-	double ans = euler(f, mas, n, step);
-	cout << ans<<endl;
-	ans = runge(f, mas, n, step);
-	cout << ans << endl;
-
-/*
-	for (int i(1); i < 100; i++)
-	{
-		double* mas = net(0, 3, i);
-		double step = h(i, mas);
-		double* mas1 = net(0, 3, i + 1);
-		double step1 = h(i + 1, mas1);
-		cout << i << '\t' << step << '\t' << "delta \t" << abs((R - euler(f, mas, i, step)) / (R - euler(f, mas1, i+1, step1))) << endl;
-	}
-*/
-	system("pause");
+	euler(f, 0, 3, 0, 1, n);
+	runge(f, 0, 3, 0, 1, n);
+	return 0;
 }
